bb7206062fc915f14e9de878144686d43a63d468
Sam HEYMAN <sam.heyman@amadeus.com>
2017-12-19 14:15:04 +0100

    fixing the index page in urls.py

--- flights/urls.py
+++ flights/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url('index', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url('live_prices', views.live_prices, name='live_prices'),
     url('travel_insights', views.travel_insights, name='travel_insights'),
     url('search_results', views.search_results, name='search_results')
